7a5a0770fa73ef24a4a2587f3bb85ffd34a7e7e7
Lance-Nichols <32449269+Lance-Nichols@users.noreply.github.com>
2022-10-20 09:30:56 +1100

    code update

--- js/folio.js
+++ js/folio.js
@@ -63,7 +63,6 @@ $(document).ready(function(){
   function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
     if(n > slides.length) {
       slideIndex = 1
     }
@@ -73,9 +72,5 @@ $(document).ready(function(){
     for(i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
-    for(i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-  }
+  };

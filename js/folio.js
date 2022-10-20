b4bb893cfca30bedeb823a9f8a228bab2427a2f9
Lance-Nichols <32449269+Lance-Nichols@users.noreply.github.com>
2022-10-20 11:54:08 +1100

    responsive update

--- js/folio.js
+++ js/folio.js
@@ -1,3 +1,28 @@
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  };
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides1");
+    if(n > slides.length) {
+      slideIndex = 1
+    }
+    if(n < 1) {
+      slideIndex = slides.length
+    }
+    for(i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+  };
+
+
+
 document.getElementById("loadOpen").click();
 
 function openPortfolio(evt, openPortfolio) {
@@ -51,26 +76,3 @@ $(document).ready(function(){
         }
   ]
   });
-
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if(n > slides.length) {
-      slideIndex = 1
-    }
-    if(n < 1) {
-      slideIndex = slides.length
-    }
-    for(i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-  };
